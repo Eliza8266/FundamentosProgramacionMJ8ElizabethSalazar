4515aa71fbf1662e12d534b84593b33b63361d82
Eliza8266 <elizabeth.salazarg.col@upb.edu.co>
2026-07-30 08:15:10 -0500

    Primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí va el código fuente
+            //Variables
+            string nombre = "Elizabeth";
+            Console.WriteLine("Bienvenido"+ nombre+" al curso de Fundamentos de Programación");
+
+        }
+    }
+}
