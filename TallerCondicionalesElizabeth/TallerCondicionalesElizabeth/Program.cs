fe56cd0b1da58b9bcb63562ad72f3cb39126a732
Eliza8266 <elizabeth.salazarg.col@upb.edu.co>
2026-08-18 19:50:10 -0500

    Punto 2 personaje de juego, taller condicionales

---
+++ TallerCondicionalesElizabeth/TallerCondicionalesElizabeth/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TallerCondicionalesElizabeth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.El personaje de un juego puede disparar si cumple con las dos siguientes condiciones: si cuenta con munición y si se encuentra en estado invencible, crear un programa que:
+            //a.Permita ingresar por teclado si el personaje está en estado invencible(True).
+            //b.La cantidad de munición que tiene el personaje en el momento será calculada por el sistema por medio de un número aleatorio, para ello utilizar la clase Random de C#. 
+            // Ejemplo:
+            /*  int numero;
+              Random rnd = new Random();
+              numero = rnd.Next(1, 6);//Devuelve un número entre 0 y 5
+              Console.WriteLine(numero);
+              c.Si el estado del personaje es invencible(true) y su cantidad de munición está entre 1 y 10, mostrar un mensaje en la consola “El personaje está disparando”, en caso contrario no hacer nada.*/
+
+            Console.WriteLine("¿El personaje esta en estado invencible (Escribe True or False)");
+            string entrada = Console.ReadLine();
+            bool esInvencible;
+
+            bool.TryParse(entrada, out esInvencible);
+            
+            if(esInvencible == true)
+            { 
+
+            //b.La cantidad de munición se calcula por un número aleatorio.
+            Random rnd = new Random();
+            int municion = rnd.Next(0,11); //Devuelve un número entre 0 y 11
+
+            Console.WriteLine($"[La munición generada por el sistema:{municion}]");
+
+            //Si es invencible y su munición esta entre 1 y 10 dispara
+            if (esInvencible == true && municion>=1 && municion<=10)
+            {
+                Console.WriteLine("El personaje está disparando");
+            }
+
+            }
+
+        }
+    }
+}
---
+++ TallerCondicionalesElizabeth/TallerCondicionalesElizabeth/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("TallerCondicionalesElizabeth")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TallerCondicionalesElizabeth")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("5cb90229-4e24-41cf-8a4b-db081bee80c1")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
